b1aed204cab716f240e385b78d026205afb8e379
t1 <t1@example.com>
2020-07-30 01:17:43 +0530

    dwm blocks

---
+++ dwmblocks/dwmblocks/blocks.h
@@ -0,0 +1,8 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/suckless/dwm/dwmblocks/status/clock",						5,		13},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
